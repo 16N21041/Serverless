257d99358b3e45212076c1c7184ed76f0067482e
16N21041 <37608548+16N21041@users.noreply.github.com>
2019-08-03 11:23:18 -0700

    Add files via upload

---
+++ configinstructor.js
@@ -0,0 +1,7 @@
+window._config={
+	cognito: {
+		userPoolId: 'ap-south-1_3LD61fNtE',
+		region: 'ap-south-1',
+		clientId: '29u2mpdj25b1i4ejtne00b9d8e'
+	},
+}; 
---
+++ configstudent.js
@@ -0,0 +1,7 @@
+window._config={
+	cognito: {
+		userPoolId: 'ap-south-1_EBHmQLegb',
+		region: 'ap-south-1',
+		clientId: 'q22q34i2611ukpc60q75pa470'
+	},
+}; 
